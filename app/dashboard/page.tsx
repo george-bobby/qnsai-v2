2d67203d49bf801308ff9289ffc6390b678370a2
George Bobby <georgebobbyawesome@gmail.com>
2025-01-03 16:56:46 +0530

    boilerplate

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,7 @@
 import { UserDetails } from "../components/user-details";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
-import { CodeSwitcher } from "../components/code-switcher";
-import { LearnMore } from "../components/learn-more";
-import { Footer } from "../components/footer";
-import { ClerkLogo } from "../components/clerk-logo";
-import { NextLogo } from "../components/next-logo";
+import Footer from '../components/footer';
 
-import { DASHBOARD_CARDS } from "../consts/cards";
 
 export default async function DashboardPage() {
   return (
@@ -15,11 +10,7 @@ export default async function DashboardPage() {
         <div className="grid grid-cols-[1fr_20.5rem] gap-10 pb-10">
           <div>
             <header className="flex items-center justify-between w-full h-16 gap-4">
-              <div className="flex gap-4">
-                <ClerkLogo />
-                <div aria-hidden className="w-px h-6 bg-[#C7C7C8]" />
-                <NextLogo />
-              </div>
+        
               <div className="flex items-center gap-2">
                 <OrganizationSwitcher
                   appearance={{
@@ -41,12 +32,10 @@ export default async function DashboardPage() {
             <UserDetails />
           </div>
           <div className="pt-[3.5rem]">
-            <CodeSwitcher />
           </div>
         </div>
       </main>
-      {/* <LearnMore cards={DASHBOARD_CARDS} />
-      <Footer /> */}
+      <Footer /> 
     </>
   );
 }
