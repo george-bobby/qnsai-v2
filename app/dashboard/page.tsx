2d76d2f4a6c7c8ca782e168568e666ce464b9dcc
George Bobby <georgebobbyawesome@gmail.com>
2025-01-03 15:10:21 +0530

    clerk

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -45,8 +45,8 @@ export default async function DashboardPage() {
           </div>
         </div>
       </main>
-      <LearnMore cards={DASHBOARD_CARDS} />
-      <Footer />
+      {/* <LearnMore cards={DASHBOARD_CARDS} />
+      <Footer /> */}
     </>
   );
 }
